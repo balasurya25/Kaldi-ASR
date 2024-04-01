459ca434de148b1210f4676cfdbabbf7cc468c9b
balasurya25 <161061052+balasurya25@users.noreply.github.com>
2024-04-01 16:10:44 +0530

    Update flac2wav.sh

--- singleSpeaker_ASR/flac2wav.sh
+++ singleSpeaker_ASR/flac2wav.sh
@@ -3,13 +3,13 @@
 mkdir -p newwave/train
 
 for speaker in MC01; do
-	mkdir -p newwave/train/$speaker
+	mkdir -p newwave/train
 for((j=1; j<=365; j++)); do
 	sourceaudio="/home/balasurya/SSN_TDSC/data/moderate/$speaker/audio/${speaker}${j}.flac"
-	destaudio="newwave/train/$speaker/${speaker}${j}.wav"
+	destaudio="newwave/train/${speaker}${j}.wav"
 	ffmpeg -i "$sourceaudio" "$destaudio"
 done
-cp /home/balasurya/SSN_TDSC/data/moderate/$speaker/label/*.lab newwave/train/$speaker
+cp /home/balasurya/SSN_TDSC/data/moderate/$speaker/label/*.lab newwave/train/
 done
 
 echo "DONE"
